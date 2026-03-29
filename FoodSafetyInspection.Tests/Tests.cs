25e6a01db94bf3dc383d43b97629b09ade91b447
Winou <84401927+Winou-1@users.noreply.github.com>
2026-03-29 13:21:57 +0200

    maybe

--- FoodSafetyInspection.Tests/Tests.cs
+++ FoodSafetyInspection.Tests/Tests.cs
@@ -289,6 +289,7 @@ public class Tests
         await ctx.SaveChangesAsync();
 
         var controller = new FollowUpsController(ctx);
+        controller.TempData = MakeTempData();
         var result = await controller.Close(followUp.Id) as RedirectToActionResult;
 
         Assert.Equal("Index", result!.ActionName);
